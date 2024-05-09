682b31467988523a5af00387e819031129e833bd
Adrian Suarez <asuarez9382@gmail.com>
2024-05-09 08:18:48 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -18,6 +18,8 @@ db.init_app(app)
 
 api = Api(app)
 
+
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -84,15 +86,28 @@ class CheckSession(Resource):
         
         return {}, 401
 
+
+
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session['user_id']:
+             return {'error': "Unauthorized"}, 401
+         
+        articles = Article.query.filter(Article.is_member_only == 1).all()
+        return [ article.to_dict() for article in articles ]
+         
+         
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session['user_id']:
+             return {'error': "Unauthorized"}, 401
+
+        article = Article.query.filter(Article.id == id).first()
+        
+        return article.to_dict()
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
